452b95ae1a463383dcc38dc69406b0c5b40349e0
Samir <devlesko3@gmail.com>
2024-11-02 16:15:11 +0200

    Added sound for the click on the buttons and deleted sound "bird" if button "=" was clicked.

--- scripts/calc.js
+++ scripts/calc.js
@@ -19,6 +19,7 @@ let isSign = true;
 for (item of buttons) {
   item.addEventListener("click", (e) => {
     buttonText = e.target.innerText;
+    new Audio("sounds/click.mp3").play();
     if (buttonText == "X" && !isSign) {
       if (flag == 1) {
         flag = 0;
@@ -37,7 +38,6 @@ for (item of buttons) {
       isSign = true;
     } else if (buttonText == "=") {
       checkForBracketMulti();
-      new Audio("sounds/bird.mp3").play();
       if (parseFloat(screen.value) < 0) {
         screen.classList.add("negative");
       } else {
